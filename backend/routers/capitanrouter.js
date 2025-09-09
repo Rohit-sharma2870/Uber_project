ecf5f9d7de81b5e1db86bb30b15df46fefc28fc3
rohit sharma <rohitbral5212@gmail.com>
2025-09-09 23:13:45 +0530

    change authcheck

--- backend/routers/capitanrouter.js
+++ backend/routers/capitanrouter.js
@@ -5,5 +5,4 @@ capitanrouter.post('/register',capitancontroller.register)
 capitanrouter.post('/login',capitancontroller.postlogin)
 capitanrouter.post('/logout',capitancontroller.logout)
 capitanrouter.post("/profile",capitancontroller.getprofile)
-capitanrouter.get("/authcheck",capitancontroller.authcheck)
 module.exports=capitanrouter
