55c7f78c9500e342733de1eb8ba18cfbc47f24f1
rohit sharma <rohitbral5212@gmail.com>
2025-09-09 17:41:10 +0530

    cors issue fixed

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://uberfront.netlify.app"],
     credentials: true,
   })
 );
--- backend/socket.js
+++ backend/socket.js
@@ -1,15 +1,15 @@
 // socket.js
-const Server=require('socket.io')
-const usermodel=require('./models/usermodel')
-const capitanmodel=require('./models/capitan-model')
+const Server = require("socket.io");
+const usermodel = require("./models/usermodel");
+const capitanmodel = require("./models/capitan-model");
 let io; // will hold the Socket.IO instance
- function initializeSocket(server) {
+function initializeSocket(server) {
   io = Server(server, {
     cors: {
-            origin: [
-            "http://localhost:5174", 
-        "http://localhost:5173", 
-        "https://uber-frontend.onrender.com"
+      origin: [
+        "http://localhost:5174",
+        "http://localhost:5173",
+        "https://uberfront.netlify.app",
       ],
       methods: ["GET", "POST"],
     },
@@ -19,49 +19,55 @@ let io; // will hold the Socket.IO instance
     console.log("New client connected:", socket.id);
 
     // Example: listen for messages
-    socket.on("join",async (data) =>{
-        const{userid,usertype}=data;
-        if(usertype=='user'){
-            await usermodel.findOneAndUpdate({_id:userid},{socketId:socket.id})
-        }else  if(usertype=='capitan'){
-            await capitanmodel.findOneAndUpdate({_id:userid},{socketId:socket.id})
-        }
+    socket.on("join", async (data) => {
+      const { userid, usertype } = data;
+      if (usertype == "user") {
+        await usermodel.findOneAndUpdate(
+          { _id: userid },
+          { socketId: socket.id }
+        );
+      } else if (usertype == "capitan") {
+        await capitanmodel.findOneAndUpdate(
+          { _id: userid },
+          { socketId: socket.id }
+        );
+      }
       console.log("Message received:", data);
 
       // broadcast to everyone
       io.emit("receiveMessage", data);
     });
 
-socket.on("update-locations", async (data) =>{
-  const { userid, latitude, longitude } = data;
-const lng = Number(longitude);
-const lat = Number(latitude);
-if (!isNaN(lng) && !isNaN(lat)) {
-  await capitanmodel.findOneAndUpdate(
-    { _id: userid },
-    {
-      $set: {
-        location: {
-          type: "Point",
-          coordinates: [lng, lat]
-        }
+    socket.on("update-locations", async (data) => {
+      const { userid, latitude, longitude } = data;
+      const lng = Number(longitude);
+      const lat = Number(latitude);
+      if (!isNaN(lng) && !isNaN(lat)) {
+        await capitanmodel.findOneAndUpdate(
+          { _id: userid },
+          {
+            $set: {
+              location: {
+                type: "Point",
+                coordinates: [lng, lat],
+              },
+            },
+          },
+          { new: true }
+        );
+      } else {
+        console.error("Invalid coordinates:", longitude, latitude);
       }
-    },
-    { new: true }
-  );
-} else {
-  console.error("Invalid coordinates:", longitude, latitude);
-}
-});
+    });
     socket.on("disconnect", () => {
       console.log("Client disconnected:", socket.id);
     });
   });
 }
 
- function sendMessageToSocketId(socketId, messageobject) {
+function sendMessageToSocketId(socketId, messageobject) {
   if (io) {
-    io.to(socketId).emit(messageobject.event,messageobject.data);
+    io.to(socketId).emit(messageobject.event, messageobject.data);
   } else {
     console.error("Socket.io not initialized!");
   }
